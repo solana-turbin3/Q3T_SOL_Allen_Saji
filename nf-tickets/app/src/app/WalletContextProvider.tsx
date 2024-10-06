21f0e4b0211cc9abf8edbb588454433b8237bde6
Allen-Saji <allensaji04@gmail.com>
2024-10-06 12:50:04 +0530

    add wallet adapter

---
+++ nf-tickets/app/src/app/WalletContextProvider.tsx
@@ -0,0 +1,27 @@
+import React, { FC, ReactNode } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+
+require("@solana/wallet-adapter-react-ui/styles.css"); // Wallet styles
+
+const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const network = clusterApiUrl("devnet"); // or 'mainnet-beta' depending on your use case
+
+  // Initialize Solana wallet (e.g., Phantom)
+  const wallets = [new PhantomWalletAdapter()];
+
+  return (
+    <ConnectionProvider endpoint={network}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletContextProvider;
